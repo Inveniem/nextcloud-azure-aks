0fd02fbaf7f665271ef16fd2902df12ea7a02bc2
Guy Elsmore-Paddock <guy@inveniem.com>
2019-03-04 12:30:22 -0500

    Fix Storage Account Removal Command
    This change was missed in f5b74f3. Without this change, removing the storage account inadvertently removes the entire resource group.

--- nextcloud-aks/delete_storage_account.sh
+++ nextcloud-aks/delete_storage_account.sh
@@ -27,7 +27,7 @@ echo ""
 echo "Removing Storage Account '${STORAGE_ACCOUNT_NAME}'..."
 echo ""
 echo "WARNING: This will delete ALL files Nextcloud has stored on Azure."
-az group delete \
+az storage account delete \
     --name "${STORAGE_ACCOUNT_NAME}" \
     --resource-group "${STORAGE_RESOURCE_GROUP}" \
     && echo "Done." || echo "Skipped."
