a91a7fefc0655e4b5ccc1f3f63a0a463a7ff2c65
Guy Elsmore-Paddock <guy@inveniem.com>
2019-03-04 21:30:17 -0500

    Add Limited Support for Kubernetes Namespaces
    This isn't complete support, but it does provide YMMV support for setting up dev and live namespaces that the admin can perform a more hands-on deployment into. Also removed one case where we were inexplicably forcing the namespace to `default` for the Nextcloud configmap.

---
+++ nextcloud-aks/delete_namespaces.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+##
+# This optional script removes all Nextcloud namespaces.
+#
+# This only works if Namespaces were previously deployed via:
+# ```
+# ./deploy_namespaces.sh
+# ```
+#
+# @author Guy Elsmore-Paddock (guy@inveniem.com)
+# @copyright Copyright (c) 2019, Inveniem
+# @license GNU AGPL version 3 or any later version
+#
+
+set -e
+set -u
+
+source './config.env'
+
+FILES=(
+    'nextcloud-namespace-dev.yaml'
+    'nextcloud-namespace-live.yaml'
+)
+
+echo "Removing Nextcloud namespaces..."
+for file in "${FILES[@]}"; do
+    kubectl delete -f "${file}"
+done
+echo "Done."
+echo ""
