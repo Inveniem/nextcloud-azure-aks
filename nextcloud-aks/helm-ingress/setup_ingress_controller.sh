e3bb6350ac5a53b5e6fdf73e137621e3abbb7257
Guy Elsmore-Paddock <guy@inveniem.com>
2019-03-05 00:45:44 -0500

    Fix "An unknown error has occurred" for Chunked Uploads > 1 MB
    This adjusts the ingress controller to be able to take chunks up to 512 MB. Since we're using chunked uploads, Nextcloud usually sends in chunks in 10 MB increments, so there's really no reason to have to force NGinx to have to deal with 1 GB+ file uploads per request. So, I've standardized everything to top out at 512 MB *per request*.
    
    Based on comments in this issue, it appears that Nextcloud has effectively no maximum file size beyond the underlying storage medium and PHP version (32-bit vs 64-bit):
    https://github.com/nextcloud/server/issues/8445
    
    Basically, the 512 MB maximum upload setting in "Basic settings" is a red-herring. It appears not to affect anything.

--- nextcloud-aks/helm-ingress/setup_ingress_controller.sh
+++ nextcloud-aks/helm-ingress/setup_ingress_controller.sh
@@ -32,7 +32,8 @@ helm install --tls \
     stable/nginx-ingress \
     --namespace "${INGRESS_NAMESPACE}" \
     --set 'controller.replicaCount=2' \
-    --set 'controller.service.externalTrafficPolicy=Local'
+    --set 'controller.service.externalTrafficPolicy=Local' \
+    --set-string controller.config.proxy-body-size="512M"
 
 # Public IP address of your ingress controller
 for attempt in {1..20}; do
