443f6e3714bc9117eee0f023d92fa18c1410a477
Guy Elsmore-Paddock <guy@inveniem.com>
2019-11-12 14:14:02 -0500

    Fix a Small Typo

--- docker/nextcloud-common/config/logger.config.php
+++ docker/nextcloud-common/config/logger.config.php
@@ -1,5 +1,5 @@
 <?php
-	# Don't write logs to file shares; output the data to emphemeral storage
+	# Don't write logs to file shares; output the data to ephemeral storage
 	# that gets echoed out to the Pod log.
 	$CONFIG = array(
 		'logfile' => '/var/log/nextcloud.log',
