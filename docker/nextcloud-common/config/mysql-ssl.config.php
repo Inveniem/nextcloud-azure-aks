8ace4ab1c4b54f3cd8d4fafab27377766e38ffca
Guy Elsmore-Paddock <guy@inveniem.com>
2019-11-13 21:03:56 -0500

    Switch Base of FPM Container to Alpine Linux
    Trying to cut down the footprint of these images, for enhanced performance.

--- docker/nextcloud-common/config/mysql-ssl.config.php
+++ docker/nextcloud-common/config/mysql-ssl.config.php
@@ -1,7 +1,24 @@
 <?php
-	# Support connecting to Azure MySQL over SSL
-	$CONFIG = array(
-		'dbdriveroptions' => array(
-			PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/Baltimore_CyberTrust_Root.pem',
-		),
-	);
+	$all_ssl_ca_locations = [
+		# Location of Azure Trusted CA on Alpine Linux
+		'/etc/ssl/certs/Baltimore_CyberTrust_Root.pem',
+
+		# Location of Azure Trusted CA on Ubuntu Linux
+		'/etc/ssl/certs/ca-cert-Baltimore_CyberTrust_Root.pem',
+	];
+
+	foreach ($all_ssl_ca_locations as $location) {
+		if (is_file($location)) {
+			$ssl_ca_location = $location;
+			break;
+		}
+	}
+
+	if (!empty($ssl_ca_location)) {
+		# Support connecting to Azure MySQL over SSL
+		$CONFIG = array(
+			'dbdriveroptions' => array(
+				PDO::MYSQL_ATTR_SSL_CA => $ssl_ca_location,
+			),
+		);
+	}
