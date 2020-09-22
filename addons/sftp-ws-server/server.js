8a91ec29bb96798ff4cf62095c2e0d15e6227f03
Guy Elsmore-Paddock <guy@inveniem.com>
2020-09-22 01:21:07 -0400

    [ARKNG-175] Re-factor + Support Path Filtering by JWT Claims
    Now, all of the logic of our implementation is contained within
    `JwtScopedSftpServer`. The server now uses the custom `authorized_paths`
    claim in the JWT to control what paths the user is allowed to see. We
    create a custom filtered filesystem per session from the JWT claims. If
    a JWT is missing this claim, the user is not allowed to authenticate.

--- addons/sftp-ws-server/server.js
+++ addons/sftp-ws-server/server.js
@@ -8,7 +8,8 @@ const url                 = require('url');
 const util                = require('util');
 const SFTP                = require("@inveniem/sftp-ws");
 
-const FilteredFilesystem = require('./FilteredFilesystem');
+const FilteredFilesystem  = require('./lib/FilteredFilesystem');
+const JwtScopedSftpServer = require('./lib/JwtScopedSftpServer');
 
 //==============================================================================
 // Constants
@@ -16,104 +17,9 @@ const FilteredFilesystem = require('./FilteredFilesystem');
 // Specify host and port for the HTTP + SFTP server WebSocket endpoint.
 const APP_HOSTNAME    = 'localhost';
 const APP_PORT        = (process.env.SFTP_WS_PORT || 4002);
+const APP_HOST        = APP_HOSTNAME + ":" + APP_PORT;
 const APP_ENDPOINT    = '/sftp';
 const JWT_HMAC_SECRET = process.env.JWT_HMAC_SECRET;
-
-//==============================================================================
-// Internal Functions
-//==============================================================================
-/**
- * Validates the origin of an incoming SFTP-WS connection request.
- *
- * @param {string} origin
- *   The 'Origin' header that was provided in the request.
- *
- * @returns {boolean}
- *   true if the origin is allowed; or, false, if it is not.
- */
-function validateOrigin(origin) {
-  let isAllowedOrigin = false;
-
-  for (let originString of ALLOWED_ORIGIN_REGEXES) {
-    let originRegex = new RegExp(originString);
-
-    if (origin.match(originRegex)) {
-      isAllowedOrigin = true;
-      break;
-    }
-  }
-
-  return isAllowedOrigin;
-}
-
-/**
- * Validates the JWT of an incoming SFTP-WS connection request.
- *
- * @param {string} origin
- *   The 'Origin' header that was provided in the request.
- * @param {string} token
- *   The JSON Web Token that was provided in the query-string of the request.
- *
- * @returns {boolean}
- *   true if the JWT grants access to this app; or, false, if it does not.
- */
-function validateJwt(origin, token) {
-  const appHost    = util.format('%s:%s', APP_HOSTNAME, APP_PORT);
-  let   isValidJwt;
-
-  if (!token) {
-    return false;
-  }
-
-  try {
-    // noinspection JSUnresolvedVariable
-    jwt.verify(
-      token,
-      Buffer.from(JWT_HMAC_SECRET, 'base64'),
-      {
-        audience: appHost,
-        issuer:   origin
-      }
-    );
-
-    isValidJwt = true;
-  }
-  catch (ex) {
-    if (ex instanceof JsonWebTokenError) {
-      console.error("JWT validation failed (" + ex + ") - JWT: " + token);
-    }
-    else {
-      throw ex;
-    }
-  }
-
-  return isValidJwt;
-}
-
-/**
- * Verifies that a particular client should be allowed to connect to this app.
- *
- * @param {RequestInfo} requestInfo
- *   The incoming WebSocket connection request and header information.
- *
- * @returns {boolean}
- *   `true` if the client connection is allowed; or, `false`, if they should be
- *   refused.
- */
-function authenticateClient(requestInfo) {
-  const requestOrigin   = requestInfo.origin,
-        isAllowedOrigin = validateOrigin(requestOrigin);
-  let   isValidToken    = false;
-
-  if (isAllowedOrigin) {
-    const queryParams = url.parse(requestInfo.req.url, true).query,
-          token       = queryParams.token;
-
-    isValidToken = validateJwt(requestOrigin, token);
-  }
-
-  return isValidToken;
-}
 
 //==============================================================================
 // Main Body
@@ -131,25 +37,21 @@ app.use(express.static(__dirname + '/client'));
 //
 // NOTE: Though "Origin" is not spoof-able in a browser, it is totally
 // spoof-able via CLI tools.
-const ALLOWED_ORIGIN_REGEXES =
-  JSON.parse(process.env.ALLOWED_ORIGIN_REGEXES || '[]');
-
-const virtualRoot = __dirname + '/files';
-
-const fileSystem =
-  new FilteredFilesystem(virtualRoot, ['client-a', 'client-b']);
+const ALLOWED_ORIGINS = JSON.parse(process.env.ALLOWED_ORIGINS || '[]');
 
 // Create an HTTP server to handle protocol switching.
 const server = http.createServer(app);
 
 // Start SFTP over WebSockets server.
-const sftp = new SFTP.Server({
-  server:       server,
-  filesystem:   fileSystem,
-  virtualRoot:  virtualRoot,
-  path:         APP_ENDPOINT,
-  verifyClient: authenticateClient,
-  log:          console // log to console
+const sftp = new JwtScopedSftpServer(
+  APP_HOST,
+  ALLOWED_ORIGINS,
+  JWT_HMAC_SECRET,
+  {
+  server:      server,
+  virtualRoot: __dirname + '/files',
+  path:        APP_ENDPOINT,
+  log:         console // log to console
 });
 
 // Start accepting requests.
